c32abb532e89e2515d7a2a2ac68bb9288a2438bf
IsmailAvayil <ismailavayil@gmail.com>
2023-06-12 20:03:55 +0530

    Updates

---
+++ precache-manifest.fc8c56be45ee229772f80e9f70bc6286.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "82abdae9bcc8aa5aa67b",
+    "url": "/products-website/static/css/main.cebc81df.chunk.css"
+  },
+  {
+    "revision": "82abdae9bcc8aa5aa67b",
+    "url": "/products-website/static/js/main.82abdae9.chunk.js"
+  },
+  {
+    "revision": "a84cb3bfebafb50804e2",
+    "url": "/products-website/static/css/1.d327aa41.chunk.css"
+  },
+  {
+    "revision": "a84cb3bfebafb50804e2",
+    "url": "/products-website/static/js/1.a84cb3bf.chunk.js"
+  },
+  {
+    "revision": "7d61181219f71c52a137",
+    "url": "/products-website/static/js/runtime~main.7d611812.js"
+  },
+  {
+    "revision": "adf535a256d1bcc179333a75d3746a26",
+    "url": "/products-website/index.html"
+  }
+];
